1de9fa0b4c8586e4bf1fe11b4457363084630b0e
dahalkumar <kumardahal536@gmail.com>
2023-10-06 21:09:43 -0400

    Project setup is updated

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-import setuptools
+from setuptools import setup, find_packages
+from typing import List
 #read the content from Readme.md file and give the description
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
@@ -11,18 +12,31 @@ AUTHOR_USER_NAME = "k17hawk"
 SRC_REPO = "finance-complaint"
 AUTHOR_EMAIL = "kumardahal536@gmail.com"
 
-setuptools.setup(
-    name=SRC_REPO,
+DESRCIPTION = "This is a sample for industry ready solution"
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+HYPHEN_E_DOT ="-e ."
+
+def get_requirements_list(file_path=REQUIREMENT_FILE_NAME) -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(file_path) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
+setup(
+    name=REPO_NAME,
     version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="",
-    long_description=long_description,
-    long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    author=AUTHOR_EMAIL,
+    description=DESRCIPTION,
+    packages=find_packages(),
+    install_requires=get_requirements_list()
 )
